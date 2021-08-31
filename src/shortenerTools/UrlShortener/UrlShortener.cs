f12731ac3dfb5a4ee5505950f50bc9743456f682
Ravi Veepu <ravi.veepu@gaf.com>
2021-08-31 10:28:42 -0400

    Disable claims auth on shortner direct api access

--- src/shortenerTools/UrlShortener/UrlShortener.cs
+++ src/shortenerTools/UrlShortener/UrlShortener.cs
@@ -54,17 +54,17 @@ namespace Cloud5mins.Function
 
             try
             {
-                var invalidRequest = Utility.CatchUnauthorize(principal, log);
+                // var invalidRequest = Utility.CatchUnauthorize(principal, log);
 
-                if (invalidRequest != null)
-                {
-                    return invalidRequest;
-                }
-                else
-                {
-                    userId = principal.FindFirst(ClaimTypes.GivenName).Value;
-                    log.LogInformation("Authenticated user {user}.", userId);
-                }
+                // if (invalidRequest != null)
+                // {
+                //     return invalidRequest;
+                // }
+                // else
+                // {
+                //     userId = principal.FindFirst(ClaimTypes.GivenName).Value;
+                //     log.LogInformation("Authenticated user {user}.", userId);
+                // }
 
                 // Validation of the inputs
                 if (req == null)
